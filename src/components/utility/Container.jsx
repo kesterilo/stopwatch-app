22427ed04be50c0437fcc17eb25395864251ba6d
Kester <ilokester@gmail.com>
2024-11-25 13:12:36 +0100

    optimized the interface for mobile screens

--- src/components/utility/Container.jsx
+++ src/components/utility/Container.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Container = ({ children }) => {
   return (
-		<div className="container">
-			{children[0]}
-      <div className="counterCard">
-        {children[1]}
-      </div>
-			{children[2]}
+		<div className="container-wrapper">
+			<div className="container">
+				{children[0]}
+				<div className="counterCard">
+					{children[1]}
+				</div>
+				{children[2]}
+			</div>
 		</div>
 	);
 }
